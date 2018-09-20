2efd47f13913d856077bf5a2d675d342ef5dbcfe
Chad Fraser <chad.fraser@mail.mcgill.ca>
2018-09-20 17:51:04 -0600

    Make the program more general by including the option to search for an item.

--- amazon.py
+++ amazon.py
@@ -1,5 +1,7 @@
 import requests
 from lxml import html
+import time
+import logging
 
 
 def parse_amazon(url, header):
@@ -44,6 +46,7 @@ def convert_cost_to_float(base_cost, shipping_cost):
     base_cost_list = list(map(str.strip, base_cost))
     shipping_cost_list = list(map(str.strip, shipping_cost))
     temp = []
+
     for cost in base_cost_list:
         cost = float(cost.split()[-1])
         temp.append(cost)
@@ -60,18 +63,65 @@ def convert_cost_to_float(base_cost, shipping_cost):
     return base_cost_list, shipping_cost_list
 
 
-# url = 'https://www.amazon.ca/gp/offer-listing/B01MUAGZ49'
-url_1 = 'https://www.amazon.ca/gp/offer-listing/B01MUAGZ49/ref=olp_f_usedVeryGood&f_new=true&f_collectible=true&' \
-        'f_usedLikeNew=true&f_usedVeryGood=true'
-url_2 = 'https://www.amazon.ca/gp/offer-listing/B01LTHP2ZK/ref=olp_f_usedVeryGood&f_new=true&f_collectible=true&' \
-        'f_usedLikeNew=true&f_usedVeryGood=true'
+def get_search_results(keyword, header):
+    result_xpath = ".//h2[contains(@class, 's-access-title')]/text()"
+    price_xpath = ".//span[contains(@class, 'a-color-price')]/text() | " \
+                  ".//span[contains(@class, 'a-text-strike') and contains(@aria-label, 'Suggested Retail')]/text()"
+    link_xpath = ".//a[contains(@class, 's-color-twister-title-link')]/@href"
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
-prices = parse_amazon(url_1, headers)
-print(prices)
+    search_url = f"https://www.amazon.ca/s/field-keywords={keyword.lower()}"
+    response = requests.get(search_url, headers=header)
+    parser = html.fromstring(response.text)
+    results = parser.xpath("//div[contains(@class, 's-item-container') and not "
+                           "(descendant::*[contains(@class, 's-sponsored-header')])]")
+    if not results:
+        print("Sorry, we didn't find any results for that.")
+        return None, None, None
+
+    print(len(results))
+    result_names = []
+    result_prices = []
+    result_links = []
+    for result_data in results:
+        data = result_data.xpath(result_xpath)
+        if data:
+            result_names.append(data[0])
+        data = result_data.xpath(price_xpath)
+        if data:
+            result_prices.append(data[0])
+        data = result_data.xpath(link_xpath)
+        if data:
+            result_links.append(data[0])
+    print(len(result_names), len(result_prices), len(result_links))
+    print(result_prices)
+    return result_names, result_prices, result_links
+
+
+if __name__ == "__main__":
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+
+    while True:
+        search_parameter = input("Please enter the name of the item you want to search for. ")
+        names, prices, links = get_search_results(search_parameter, headers)
+        if names:
+            break
+    for num, (item, val, link) in enumerate(zip(names, prices, links)):
+        print(f"{num+1:02d}. ({val}) {item if len(item) < 50 else item[:50] + '...'}")
+    input()
+
+#     # url = 'https://www.amazon.ca/gp/offer-listing/B01MUAGZ49'
+#     url_1 = 'https://www.amazon.ca/gp/offer-listing/B01MUAGZ49/ref=olp_f_usedVeryGood&f_new=true&f_collectible=true&' \
+#             'f_usedLikeNew=true&f_usedVeryGood=true'
+#     url_2 = 'https://www.amazon.ca/gp/offer-listing/B01LTHP2ZK/ref=olp_f_usedVeryGood&f_new=true&f_collectible=true&' \
+#             'f_usedLikeNew=true&f_usedVeryGood=true'
+#     prices = parse_amazon(url_1, headers)
+#     prices = parse_amazon(url_2, headers)
 
 # ref=sr_nr_p_n_shipping_option-bin_1
 # olpOfferPrice
 # olpShippingPrice
 # olpCondition
 # olpSellerColumn
+#smo x2, tomotoc, botw, mk8 deluxe, joycon l/r,
+# Super Mario Party missing price
+# tomotoc missing all
